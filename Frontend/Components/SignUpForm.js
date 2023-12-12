7e0d9a9bdb27243a7856d094bd33524f9acc627f
SanketSonawane11 <sanketsonawane424@gmail.com>
2023-12-12 18:33:57 +0530

    Form Started: First + Last name -> Done

--- Frontend/Components/SignUpForm.js
+++ Frontend/Components/SignUpForm.js
@@ -1,14 +1,37 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 
 
 function SignUpForm() {
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
   return (
     <div className='signUpForm'>
 
-      <form>
+      <form className='mainForm'>
 
-        <h2>First name: </h2> <input type='text'  required/>
+        <div className='inputRow2'>
+
+          <div className='field1'>
+
+            <h4 className='fieldName'>First name: </h4> <input className='fieldValue' type='text' placeholder='Name' onChange={(ip)=>
+            {
+              setFirstName(ip.target.value);
+            }} required/>
+
+          </div>
+
+          <div className='field1'>
+
+            <h4 className='fieldName'>Last name: </h4> <input className='fieldValue' type='text' placeholder='Last Name' onChange={(ip)=>
+            {
+              setLastName(ip.target.value);
+            }} required/>
+
+          </div>
+          
+        </div>
 
       </form>
 
