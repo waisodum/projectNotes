528561457579f151354051b398e3cbecf1aea57c
sarvesh0202 <sarveshparab002@gmail.com>
2023-12-13 22:15:07 +0530

    baign 2

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const userModel = require('../models/userSchema');
-const { hashSync } = require('bcrypt');
+// const user = require('../models/userSchema');
+// const { hashSync } = require('bcrypt');
 
 
 
@@ -14,28 +14,28 @@ router.post('/', function(req, res, next) {
 router.get('/',(req,res,next)=>{
   res.send('hi')
 })
-router.post('/Register',(req,res)=>{
-  const user = new userModel({
-    username : req.body.username,
-    password : hashSync(req.body.password,10)
-  })
-  user.save().then(user=>{
-    res.send({
-      success: true,
-      message: "User created successfully",
-      user: {
-        id : user._id,
-        username : user.username
-      }
-    })
-  }).catch(err=>{
-    res.send({
-      success: false,
-      message: "Something went wrong",
-     error: err
-    })
+// router.post('/Register',(req,res)=>{
+//   const user = new user({
+//     username : req.body.username,
+//     password : hashSync(req.body.password,10)
+//   })
+//   user.save().then(user=>{
+//     res.send({
+//       success: true,
+//       message: "User created successfully",
+//       user: {
+//         id : user._id,
+//         username : user.username
+//       }
+//     })
+//   }).catch(err=>{
+//     res.send({
+//       success: false,
+//       message: "Something went wrong",
+//      error: err
+//     })
 
-  })
-})
+//   })
+// })
 
 module.exports = router;
