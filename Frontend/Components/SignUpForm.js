1a2922a80799ccffcc3d29219523f6b9ed6becf6
SanketSonawane11 <sanketsonawane424@gmail.com>
2024-03-07 21:14:59 +0530

    Final COmmit

--- Frontend/Components/SignUpForm.js
+++ Frontend/Components/SignUpForm.js
@@ -1,5 +1,5 @@
+'use client'
 
-import { data } from 'autoprefixer'
 import axios from 'axios'
 import { useRouter } from 'next/navigation'
 import React, { useState } from 'react'
@@ -11,15 +11,14 @@ function SignUpForm() {
   const [username, setUsername] = useState('');
   const [year, setYear] = useState('');
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const router =useRouter();
+  const [password, setPassword] = useState('');
+  const router = useRouter();
 
   const changeBranch = (bra) => {
     setBranch(bra.target.value);
   }
 
-  const changeYear = (yea)=>
-  {
+  const changeYear = (yea) => {
     setYear(yea.target.value);
   };
 
@@ -30,39 +29,39 @@ function SignUpForm() {
     e.preventDefault();
     var userData = {
       firstname: firstName,
-      lasname:lastName,
+      lasname: lastName,
       email,
       branch,
       year,
       username,
       password,
     };
-try
-{    var GG = await axios.post("http://localhost:8000/Register", userData);
-  
-if (!GG.data.success) {
-  setUsername("");
-  setEmail("")
-  alert(GG.data.message);
-  return null
-} 
-else {
-  router.push('/notesPage')
-  setEmail("");
-  setFirstName("");
-  setLastName("");
-  setPassword("");
-  setUsername("");
-  setBranch("");
-  setYear("");
-  return null
-}
-}  
-catch(err){
-alert('There is some error in server. Plz contact us')
-console.log(err);
-  return null;
-}
+    try {
+      var GG = await axios.post("http://localhost:8000/Register", userData);
+
+      if (!GG.data.success) {
+        setUsername("");
+        setEmail("")
+        alert(GG.data.message);
+        return null
+      }
+      else {
+        router.push('/notesPage')
+        setEmail("");
+        setFirstName("");
+        setLastName("");
+        setPassword("");
+        setUsername("");
+        setBranch("");
+        setYear("");
+        return null
+      }
+    }
+    catch (err) {
+      alert('There is some error in server. Plz contact us')
+      console.log(err);
+      return null;
+    }
 
 
   }
@@ -76,12 +75,11 @@ console.log(err);
 
           <div className='field1'>
 
-            <h4 className='fieldName'>First name: </h4> <input className='fieldValue' type='text' value={firstName} placeholder='Name' onChange={(ip)=>
-            {
+            <h4 className='fieldName'>First name: </h4> <input className='fieldValue' type='text' value={firstName} placeholder='Name' onChange={(ip) => {
               setFirstName(ip.target.value);
-            }} required/>
+            }} required />
 
-          </div>  
+          </div>
           <div className="field1">
             <h4 className="fieldName">Last name: </h4>{" "}
             <input
@@ -95,14 +93,14 @@ console.log(err);
               required
             />
           </div>
-          
+
         </div>
 
         <div className='inputRow2'>
 
           <div className='field1'>
 
-            <h4 className='fieldName'>Branch: </h4> 
+            <h4 className='fieldName'>Branch: </h4>
             <select className='fieldValue' value={branch} onChange={changeBranch} required>
 
               <option value='' disabled> Branch? </option>
@@ -118,7 +116,7 @@ console.log(err);
 
           <div className='field1'>
 
-            <h4 className='fieldName'>Year: </h4> 
+            <h4 className='fieldName'>Year: </h4>
             <select className='fieldValue' onChange={changeYear} required>
 
               <option value='' selected disabled>Year?</option>
@@ -130,36 +128,33 @@ console.log(err);
             </select>
 
           </div>
-          
+
         </div>
         <div className='field1'>
 
-<h4 className='fieldName'>email: </h4> <input className='fieldValue' value={email} type='text' placeholder='Set a new username' onChange={async (ip)=>
-{
-  setEmail(ip.target.value);
+          <h4 className='fieldName'>email: </h4> <input className='fieldValue' value={email} type='text' placeholder='Set a new username' onChange={async (ip) => {
+            setEmail(ip.target.value);
 
-}} required/>
+          }} required />
 
-</div>
+        </div>
 
-          <div className='field1'>
+        <div className='field1'>
 
-            <h4 className='fieldName'>Username: </h4> <input className='fieldValue' value={username} type='text' placeholder='Set a new username' onChange={async (ip)=>
-            {
-              setUsername(ip.target.value);
-            
-            }} required/>
+          <h4 className='fieldName'>Username: </h4> <input className='fieldValue' value={username} type='text' placeholder='Set a new username' onChange={async (ip) => {
+            setUsername(ip.target.value);
 
-          </div>
+          }} required />
 
-          <div className='field1'>
+        </div>
 
-            <h4 className='fieldName'>Password: </h4> <input className='fieldValue w-[18vw]' value={password} type='password' placeholder='Needs to be strong' onChange={(ip)=>
-            {
-              setPassword(ip.target.value);
-              
-            }} required/>
-</div>
+        <div className='field1'>
+
+          <h4 className='fieldName'>Password: </h4> <input className='fieldValue w-[18vw]' value={password} type='password' placeholder='Needs to be strong' onChange={(ip) => {
+            setPassword(ip.target.value);
+
+          }} required />
+        </div>
         <button className="createButton" type="submit">
           Create account
         </button>
